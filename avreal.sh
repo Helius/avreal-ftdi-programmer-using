6f08faa70b1796e21b6a0d5471efc3ab17a5a278
Eugene Samoylov <e.samoylov@2Gis.ru>
2015-10-06 23:19:43 +0600

    force set lib symlink to suitable arch

--- avreal.sh
+++ avreal.sh
@@ -15,7 +15,7 @@ else
 		ARCH=86
 fi
 
-ln -s x$ARCH/libftd2xx.so.1.3.6 libftd2xx.so.0 2>/dev/null
+ln -sf x$ARCH/libftd2xx.so.1.3.6 libftd2xx.so.0 2>/dev/null
 export LD_LIBRARY_PATH=$(pwd):$LD_LIBRARY_PATH
 
 
